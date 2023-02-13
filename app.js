b8e58675b92c0d526d2f0fde7aaec3c86c0b4774
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-02-13 13:37:51 -0800

    Create tree factory function
    The function takes an array, sorts it and removes duplicate entries. It
    then creates a balanced, binary search tree.

--- app.js
+++ app.js
@@ -21,3 +21,40 @@ const nodeFactory = (value = null, leftChildren = null, rightChildren = null) =>
     getValue, getLeftChildren, getRightChildren, setValue, setLeftChildren, setRightChildren,
   };
 };
+
+const treeFactory = (array) => {
+  const sortedArray = [...new Set(array.sort((a, b) => a - b))];
+
+  const buildTree = (anArray) => {
+    if (anArray.length === 0) return null;
+    const midpoint = Math.floor(anArray.length / 2);
+    const newNode = nodeFactory(anArray[midpoint]);
+    newNode.setLeftChildren(buildTree(anArray.slice(0, midpoint)));
+    newNode.setRightChildren(buildTree(anArray.slice(midpoint + 1)));
+
+    return newNode;
+  };
+
+  const root = buildTree(sortedArray);
+  const getRoot = () => root;
+
+  return { getRoot };
+};
+
+const testArray = [1, 3, 1, 2, 4, 5, 8, 7, 3, 6, 8, 10];
+console.log([...new Set(testArray.sort((a, b) => a - b))]);
+const testTree = treeFactory(testArray);
+
+console.log(testTree.getRoot().getValue());
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node.getRightChildren() !== null) {
+    prettyPrint(node.getRightChildren(), `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.getValue()}`);
+  if (node.getLeftChildren() !== null) {
+    prettyPrint(node.getLeftChildren(), `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+prettyPrint(testTree.getRoot());
