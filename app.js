1c01fa0aa5b98d1ffe6a063b1bf8788ce42a5a3f
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-02-13 11:09:15 -0800

    Create node factory function

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('test');
+const nodeFactory = (value = null, leftChildren = null, rightChildren = null) => {
+  let storedValue = value;
+  let storedLeftChildren = leftChildren;
+  let storedRightChildren = rightChildren;
+
+  const getValue = () => storedValue;
+  const getLeftChildren = () => storedLeftChildren;
+  const getRightChildren = () => storedRightChildren;
+
+  const setValue = (newValue) => {
+    storedValue = newValue;
+  };
+  const setLeftChildren = (newLeftChildren) => {
+    storedLeftChildren = newLeftChildren;
+  };
+  const setRightChildren = (newRightChildren) => {
+    storedRightChildren = newRightChildren;
+  };
+
+  return {
+    getValue, getLeftChildren, getRightChildren, setValue, setLeftChildren, setRightChildren,
+  };
+};
