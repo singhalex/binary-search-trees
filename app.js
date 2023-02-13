3a185ebc7ee15d9051aeb4f0d27c535d1b806cf0
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-02-13 14:43:33 -0800

    Rename children to child

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-const nodeFactory = (value = null, leftChildren = null, rightChildren = null) => {
+const nodeFactory = (value = null, leftChild = null, rightChild = null) => {
   let storedValue = value;
-  let storedLeftChildren = leftChildren;
-  let storedRightChildren = rightChildren;
+  let storedLeftChild = leftChild;
+  let storedRightChild = rightChild;
 
   const getValue = () => storedValue;
-  const getLeftChildren = () => storedLeftChildren;
-  const getRightChildren = () => storedRightChildren;
+  const getLeftChild = () => storedLeftChild;
+  const getRightChild = () => storedRightChild;
 
   const setValue = (newValue) => {
     storedValue = newValue;
   };
-  const setLeftChildren = (newLeftChildren) => {
-    storedLeftChildren = newLeftChildren;
+  const setLeftChild = (newLeftChildren) => {
+    storedLeftChild = newLeftChildren;
   };
-  const setRightChildren = (newRightChildren) => {
-    storedRightChildren = newRightChildren;
+  const setRightChild = (newRightChildren) => {
+    storedRightChild = newRightChildren;
   };
 
   return {
-    getValue, getLeftChildren, getRightChildren, setValue, setLeftChildren, setRightChildren,
+    getValue, getLeftChild, getRightChild, setValue, setLeftChild, setRightChild,
   };
 };
 
 const treeFactory = (array) => {
   const sortedArray = [...new Set(array.sort((a, b) => a - b))];
-
   const buildTree = (anArray) => {
     if (anArray.length === 0) return null;
     const midpoint = Math.floor(anArray.length / 2);
     const newNode = nodeFactory(anArray[midpoint]);
-    newNode.setLeftChildren(buildTree(anArray.slice(0, midpoint)));
-    newNode.setRightChildren(buildTree(anArray.slice(midpoint + 1)));
+    newNode.setLeftChild(buildTree(anArray.slice(0, midpoint)));
+    newNode.setRightChild(buildTree(anArray.slice(midpoint + 1)));
 
     return newNode;
   };
@@ -38,22 +37,39 @@ const treeFactory = (array) => {
   const root = buildTree(sortedArray);
   const getRoot = () => root;
 
-  return { getRoot };
+  const insert = (value) => {
+    let currentNode = root;
+
+    if (value === currentNode.getValue()) {
+      return null;
+    }
+    if (value < currentNode.getValue()) {
+      if (currentNode.getLeftChildren === null) {
+        const newNode = nodeFactory(value);
+        currentNode.setLeftChildren(newNode);
+      } else {
+        currentNode = currentNode.getLeftChildren();
+        insert(value);
+      }
+    }
+  };
+
+  return { getRoot, insert };
 };
 
 const testArray = [1, 3, 1, 2, 4, 5, 8, 7, 3, 6, 8, 10];
 console.log([...new Set(testArray.sort((a, b) => a - b))]);
 const testTree = treeFactory(testArray);
 
-console.log(testTree.getRoot().getValue());
+console.log(testTree.getRoot().getLeftChild().getValue());
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
-  if (node.getRightChildren() !== null) {
-    prettyPrint(node.getRightChildren(), `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  if (node.getRightChild() !== null) {
+    prettyPrint(node.getRightChild(), `${prefix}${isLeft ? '│   ' : '    '}`, false);
   }
   console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.getValue()}`);
-  if (node.getLeftChildren() !== null) {
-    prettyPrint(node.getLeftChildren(), `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  if (node.getLeftChild() !== null) {
+    prettyPrint(node.getLeftChild(), `${prefix}${isLeft ? '    ' : '│   '}`, true);
   }
 };
 
